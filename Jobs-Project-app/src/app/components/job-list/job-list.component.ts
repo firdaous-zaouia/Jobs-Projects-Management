35739be6d58b7f5706f5bad5b05f9095a374fce0
Zaouia Firdaous <zaouia.firdaous@gmail.com>
2024-10-09 14:54:49 +0200

    Form jobs et Liste Jobs finaliser/ premier partie terminer

--- Jobs-Project-app/src/app/components/job-list/job-list.component.ts
+++ Jobs-Project-app/src/app/components/job-list/job-list.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { JobService,Job } from '../service/job.service';
 import { Router } from '@angular/router';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-job-list',
@@ -8,6 +9,7 @@ import { Router } from '@angular/router';
 })
 export class JobListComponent implements OnInit{
 jobs: Job[]=[];
+jobToDeleteIndex: number | null = null; // Variable pour stocker l'index du job à supprimer
 
 constructor(private jobService: JobService, private cdr: ChangeDetectorRef, private router:Router){}
 
@@ -19,11 +21,29 @@ ngOnInit(){
   });
 }
 
-deleteJob(index: number){
-  if(confirm('voulez-vous vraiment supprimer cet emploi?')){
-    this.jobService.deleteJob(index);
+openDeleteModal(index: number) {
+  this.jobToDeleteIndex = index; // Stocker l'index du job à supprimer
+  const modalElement = document.getElementById('confirmDeleteModal');
+if (modalElement) {
+  const modal = new bootstrap.Modal(modalElement); // Ouvrir la modale
+  modal.show();
+}
+}
+confirmDelete() {
+  if (this.jobToDeleteIndex !== null) {
+    this.jobService.deleteJob(this.jobToDeleteIndex); // Supprimer l'emploi
+    this.jobToDeleteIndex = null; // Réinitialiser après suppression
+
+    const modalElement = document.getElementById('confirmDeleteModal');
+    if (modalElement !== null) {
+      const modal = bootstrap.Modal.getInstance(modalElement); // Fermer la modale
+      modal?.hide(); // Vérification optionnelle si getInstance retourne null
+    } else {
+      console.error('Element with id "confirmDeleteModal" not found.');
+    }
   }
 }
+
 
 editJob(index:number){
   this.router.navigate(['/jobs/edit', index]);//page dedition d'un emploi
